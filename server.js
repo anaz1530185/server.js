e841f854b2ad46d93cb493b15c3a34a384e5b643
Anaz Bin Mannan <1530185@iub.edu.bd>
2018-07-18 03:29:54 +0000

    classwork7

--- server.js
+++ server.js
@@ -13,7 +13,7 @@
     console.log('Server running');
   });*/
 
-var http = require('http');
+ /* var http = require('http');
   var express = require('express');
   var app = express();
   var server = http.Server(app);
@@ -30,4 +30,40 @@ var http = require('http');
   
   server.listen(3000, 'localhost' ,function(){
     console.log('Server running');
+  });*/
+
+//classwork7
+
+  var http = require('http');
+  var express = require('express');
+  var app = express();
+  var bodyParser = require('body-parser');
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({extended:true}));
+  
+  
+  
+  var server = http.Server(app);
+  
+  
+  app.get('/',function(req,res)                //how files are called from data base 
+  {
+    res.sendfile(__dirname+'/index.html');
   });
+  
+  app.get('/form',function(req,res)
+  {
+    res.sendfile(__dirname+'/form.html');
+  });
+  app.post('/submit_user',function(req,res)
+  {
+    console.log(req.body);
+  });
+  app.get('/system/about',function(req,res)
+  {
+    res.sendfile(__dirname+'/about.html');
+  });
+   server.listen(process.env.PORT, process.env.IP, function(){
+    console.log('Server running');
+  });
+
